93ba968c4a6f66135de0a5eddd75b58f0ad75976
Jonas Schievink <jonas@schievink.net>
2016-04-19 00:05:29 +0200

    Pass ROM/Save via argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[macro_use] extern crate log;
+extern crate env_logger;
+extern crate clap;
 extern crate gtk;
 extern crate gdk;
 extern crate gdk_pixbuf;
@@ -11,15 +13,37 @@ mod data;
 mod view;
 mod model;
 
+use clap::{App, Arg};
+
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::path::PathBuf;
 
 fn main() {
+    env_logger::init().unwrap();
+
     gtk::init().unwrap();
+
+    let matches = App::new("Breeze Emulator Tool")
+                      .author("Jonas Schievink <jonas@schievink.net>")
+                      .about("GUI tool for introspection of Breeze save states")
+                      .arg(Arg::with_name("rom").takes_value(true))
+                      .arg(Arg::with_name("state").takes_value(true))
+                      .get_matches();
 
     let model = Rc::new(RefCell::new(model::Model::new()));
     let view = view::MainView::new(model.clone());
     let weak_view = view.get_weak_ref_to_view();
-    model.borrow_mut().set_view(weak_view);
+
+    {
+        let mut model = model.borrow_mut();
+        model.set_view(weak_view);
+        if let Some(rom) = matches.value_of("rom") {
+            model.load_rom(PathBuf::from(rom)).unwrap();    // FIXME dont unwrap
+        }
+        if let Some(state) = matches.value_of("state") {
+            model.load_save_state(PathBuf::from(state)).unwrap();
+        }
+    }
     view.main_loop();
 }
